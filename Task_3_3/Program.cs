751bab53b49059597ddcece4c6c7956086077371
dm <semu-dima@yandex.ru>
2021-02-17 23:35:42 +0300

    add 3 task

---
+++ Task_3_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task_3_3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//3 Задача
+
+			string str;
+
+			do
+			{
+				Console.Clear();
+				Console.Write("Введите \"Hello\": ");
+				str = Console.ReadLine();
+				str = str.ToLower();
+
+			} while (str != "hello");
+
+			for (int i = str.Length - 1; i >= 0; i--)
+			{
+
+				if (str[i] == 'h')
+					Console.Write(str[i].ToString().ToUpper());
+				else
+					Console.Write(str[i]);
+			}
+		}
+	}
+}
