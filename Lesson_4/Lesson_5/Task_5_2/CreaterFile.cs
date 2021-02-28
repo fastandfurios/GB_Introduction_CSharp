daa860afb52f80340482c1c179ebd8fd59519a14
dm <semu-dima@yandex.ru>
2021-02-28 23:18:02 +0300

    add Task_5_2

---
+++ Lesson_4/Lesson_5/Task_5_2/CreaterFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5.Task_5_2
+{
+    public class CreaterFile
+    {
+		private string _fileName;
+
+		public string FileName
+		{
+			get => _fileName;
+			set
+			{
+				_fileName = value;
+				WriteCurrentTime();
+			} 
+		}
+
+		private void WriteCurrentTime()
+		{
+			File.WriteAllText(_fileName, DateTime.Now.ToString());
+			File.AppendAllText(_fileName, Environment.NewLine);
+		}
+	}
+}
---
+++ Lesson_4/Lesson_5/Task_5_2/Task_2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5.Task_5_2
+{
+    public class Task_2
+    {
+        
+    }
+}
