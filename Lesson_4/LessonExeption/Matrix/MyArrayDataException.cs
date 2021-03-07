9258f8d055d93edd8557c06e41cdb973f3554015
dm <semu-dima@yandex.ru>
2021-03-07 23:34:12 +0300

    change task3

---
+++ Lesson_4/LessonExeption/Matrix/MyArrayDataException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonExeption.Matrix
+{
+    [Serializable]
+    public class MyArrayDataException : Exception
+    {
+	    private string _message;
+
+	    public MyArrayDataException(string message) : base(message) => _message = message;
+
+	    public override string ToString() => _message;
+    }
+}
