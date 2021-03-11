ea9585cda98c58838556a9ea568de75df75febca
dm <semu-dima@yandex.ru>
2021-03-11 22:34:59 +0300

    add code

---
+++ Lesson_4/CrossZero/Cross.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossZero
+{
+    public class Cross
+    {
+        static int SIZE_X = 3;
+        static int SIZE_Y = 3;
+
+	    char[,] field = new char[SIZE_Y, SIZE_X];
+
+		char PLAYER_DOT = 'X';
+        char AI_DOT = 'O';
+        char EMPTY_DOT = '.';
+
+        Random random = new Random();
+
+        public Cross()
+        {
+	        InitField();
+	        PrintField();
+
+	        while (true)
+	        {
+		        PlayerStep();
+		        PrintField();
+		        if (CheckWin(PLAYER_DOT))
+		        {
+			        Console.WriteLine("Player Win!");
+			        break;
+		        }
+		        if (IsFieldFull())
+		        {
+			        Console.WriteLine("DRAW");
+			        break;
+		        }
+		        AiStep();
+		        PrintField();
+		        if (CheckWin(AI_DOT))
+		        {
+			        Console.WriteLine("SkyNet Win!");
+			        break;
+		        }
+		        if (IsFieldFull())
+		        {
+			        Console.WriteLine("DRAW");
+			        break;
+		        }
+	        }
+        }
+
+        private void InitField()
+        {
+            for (int i = 0; i < SIZE_Y; i++)
+            {
+                for (int j = 0; j < SIZE_X; j++)
+                {
+                    field[i, j] = EMPTY_DOT;
+                }
+            }
+        }
+
+        private void PrintField()
+        {
+            Console.Clear();
+            Console.WriteLine("-------");
+            for (int i = 0; i < SIZE_Y; i++)
+            {
+                Console.Write("|");
+                for (int j = 0; j < SIZE_X; j++)
+                {
+                    Console.Write(field[i, j] + "|");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("-------");
+        }
+
+        private void SetSym(int y, int x, char sym)
+        {
+            field[y, x] = sym;
+        }
+
+        private bool IsCellValid(int y, int x)
+        {
+            if (x < 0 || y < 0 || x > SIZE_X - 1 || y > SIZE_Y - 1)
+            {
+                return false;
+            }
+
+            return field[y, x] == EMPTY_DOT;
+        }
+
+        private bool IsFieldFull()
+        {
+            for (int i = 0; i < SIZE_Y; i++)
+            {
+                for (int j = 0; j < SIZE_X; j++)
+                {
+                    if (field[i, j] == EMPTY_DOT)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void PlayerStep()
+        {
+            int x;
+            int y;
+            do
+            {
+                Console.WriteLine($"Введите координаты X Y (1-{SIZE_X})");
+                x = Int32.Parse(Console.ReadLine()) - 1;
+                y = Int32.Parse(Console.ReadLine()) - 1;
+            } while (!IsCellValid(y, x));
+            SetSym(y, x, PLAYER_DOT);
+        }
+
+        private void AiStep()
+        {
+            int x;
+            int y;
+            do
+            {
+                x = random.Next(0, SIZE_X);
+                y = random.Next(0, SIZE_Y);
+            } while (!IsCellValid(y, x));
+            SetSym(y, x, AI_DOT);
+        }
+
+        private bool CheckWin(char sym)
+        {
+            if (field[0, 0] == sym && field[0, 1] == sym && field[0, 2] == sym)
+            {
+                return true;
+            }
+            if (field[1, 0] == sym && field[1, 1] == sym && field[1, 2] == sym)
+            {
+                return true;
+            }
+            if (field[2, 0] == sym && field[2, 1] == sym && field[2, 2] == sym)
+            {
+                return true;
+            }
+
+            if (field[0, 0] == sym && field[1, 0] == sym && field[2, 0] == sym)
+            {
+                return true;
+            }
+            if (field[0, 1] == sym && field[1, 1] == sym && field[2, 1] == sym)
+            {
+                return true;
+            }
+            if (field[0, 2] == sym && field[1, 2] == sym && field[2, 2] == sym)
+            {
+                return true;
+            }
+
+            if (field[0, 0] == sym && field[1, 1] == sym && field[2, 2] == sym)
+            {
+                return true;
+            }
+            if (field[2, 0] == sym && field[1, 1] == sym && field[0, 2] == sym)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Lesson_4/CrossZero/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CrossZero
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var cross = new Cross();
+		}
+	}
+}
